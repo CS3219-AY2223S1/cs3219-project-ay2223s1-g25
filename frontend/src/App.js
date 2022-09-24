19bf2199d1cfdfa13e734590435c592d7f58f39d
kaushikkrdy <kaushikkr@hotmail.com>
2022-09-24 12:50:41 +0800

    Add socket io client setup logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,10 @@ import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom
 import SignupPage from './components/SignupPage';
 import {Box} from "@mui/material";
 
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:8001');
+
 function App() {
     return (
         <div className="App">
