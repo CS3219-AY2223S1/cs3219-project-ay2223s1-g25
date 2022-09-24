3142ddf6e7133d2e69a5d484960c945ceefc986e
Kaushik Kumar <70100131+kaushikkrdy@users.noreply.github.com>
2022-09-24 15:57:26 +0800

    Revert "Change to onInput to allow duplicate messages"
    This reverts commit 7db7b5d12dbae182ee4e5932f6220ed716059115.

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -31,7 +31,7 @@ function Chat({ socket, room }) {
       </div>
       <div className="messagebox">
         <input
-          onInput={(event) => {
+          onChange={(event) => {
             setMessage(event.target.value);
           }}
           type="text"
