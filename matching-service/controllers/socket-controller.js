adaa8e9d1c6ac866e1d3a8a5e1444ea0bfde5df4
Kaushik Kumar <70100131+kaushikkrdy@users.noreply.github.com>
2022-10-18 09:31:21 +0800

    Remove original chat service using matching-service

--- matching-service/controllers/socket-controller.js
+++ matching-service/controllers/socket-controller.js
@@ -62,11 +62,6 @@ const startSocket = (httpServer) => {
             await socket.leave(roomId);
         })
 
-        socket.on("sendMsg", (msg) => {
-            const [socketId, roomId] = socket.rooms;
-            socket.to(roomId).emit("gotMsg", msg);
-          });
-
         socket.on("leave-room", async () => {
             console.log("LEAVE ROOM");
             // delete match only if both players leave the room, otherwise remain the same
