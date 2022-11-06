af1d293da7746c2fed90d63355720dba9d3313d3
wxliong <39647325+wxliong@users.noreply.github.com>
2022-11-07 02:01:30 +0800

    Add testing for Collaboration service

--- collab-service/server.js
+++ collab-service/server.js
@@ -5,19 +5,23 @@ const io = require('socket.io')(4001, {
     },
 })
 
-    io.on("connection", socket => {
-    console.log('connected', socket.id);
+io.on("connection", socket => {
+    console.log("Connected: ", socket.id);
     io.to(socket.id).emit("connected", socket.id);
     var roomId;
+
     socket.on("signin", (data) => {
         roomId = data;
-        socket.join(roomId);});
-    socket.on("send-changes", (delta) => {
-        socket.to(roomId).emit("receive-changes", delta);
+        socket.join(roomId);
+    });
+    
+    socket.on("send-changes", (data) => {
+        socket.to(roomId).emit("receive-changes", data);
     });
 
     socket.on('disconnect', () => {
-        //alert user if other user disconnects
         console.log('Disconnected', socket.id);
     });
 })
+
+module.exports = io;
---
+++ collab-service/tests/test.js
@@ -0,0 +1,97 @@
+// Dev dependencies
+const io = require("socket.io-client");
+const app = require("../server");
+
+const initSocket = () => {
+    return new Promise((resolve, reject) => {
+      const port = 4001;
+      const socket = io(`http://localhost:${port}`, {
+        "reconnection delay": 0,
+        "reopen delay": 0,
+        "force new connection": true,
+      });
+  
+      socket.on("connected", () => {
+        resolve(socket);
+      });
+
+      setTimeout(() => {
+        reject(new Error("Failed to connect within 5 seconds."));
+      }, 5000);
+    });
+};
+
+const destroySocket = (socket) => {
+    return new Promise((resolve, reject) => {
+      // check if socket connected
+      if (socket.connected) {
+        // disconnect socket
+        socket.disconnect();
+        socket.close();
+        resolve(true);
+      } else {
+        // not connected
+        console.log("no connection to break...");
+        resolve(false);
+      }
+    });
+};
+
+describe("test suit: Collab Service", () => {
+        let socketClient, socketClient1;
+        
+    test("test: Connect two clients", async () => {
+        const serverResponse = new Promise(async (resolve, reject) => {
+            const socketC = await initSocket();
+            const socketC1 = await initSocket();
+            socketClient = socketC;
+            socketClient1 = socketC1;
+            resolve([socketC,socketC1])
+            setTimeout(() => {
+                reject(new Error("Failed to get reponse, connection timed out..."));
+            }, 5000);
+        });
+        const message  = await serverResponse;
+        expect(message[0]).not.toBe(message[1]);
+    }, 10000);
+
+    test("test: Join Same Room", async () => {
+        const serverResponse = new Promise(async (resolve, reject) => {
+            await socketClient.emit("signin", "roomid123");
+            await socketClient1.emit("signin", "roomid123");
+            var it = app.sockets.adapter.rooms.get(socketClient.id).values();
+            var first = it.next();
+            var value = first.value;
+            resolve(value);
+            setTimeout(() => {
+                reject(new Error("Failed to get reponse, connection timed out..."));
+            }, 5000);
+        });
+
+        const message  = await serverResponse;
+        expect(message).toBe(socketClient.id);
+    }, 10000);
+
+    test("test: Send Message", async () => {
+        const serverResponse = new Promise((resolve, reject) => {
+            socketClient.emit("send-changes", "this is a random string");
+            socketClient1.on("receive-changes", data => {
+            destroySocket(socketClient);
+            destroySocket(socketClient1);
+            resolve(data);
+            });
+    
+            setTimeout(() => {
+            reject(new Error("Failed to get reponse, connection timed out..."));
+            }, 5000);
+      });
+
+      const message  = await serverResponse;
+      expect(message).toBe("this is a random string");
+
+    }, 10000);
+
+    afterAll(() => {
+        app.close();
+    });
+})
